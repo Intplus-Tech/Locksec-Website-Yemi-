16cda426adca2a6ce9dd31f51a00083c36b0f905
cyberrspace <xhamooel95@gmail.com>
2025-09-17 22:49:04 +0100

    image update

--- src/app/components/IphoneImage.tsx
+++ src/app/components/IphoneImage.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function IphoneImage() {
   return (
-    <Image
-      src="/iphone-bg.svg"
-      alt=""
-      width={218}
-      height={311}
-      className="absolute -right-[1rem] lg:bottom-[8rem] lg:w-[218px] lg:h-[311px] lg:z-auto"
-    />
+    <div className="hidden lg:block">
+      <Image
+        src="/iphone-bg.svg"
+        alt=""
+        width={218}
+        height={311}
+        className="absolute -right-[1rem] lg:bottom-[8rem] lg:w-[218px] lg:h-[311px] lg:z-auto"
+      />
+    </div>
+   
   );
 }
