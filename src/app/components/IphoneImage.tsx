c61c43574d16880b380bd7b03444c628ec5bf34e
cyberrspace <xhamooel95@gmail.com>
2025-09-18 09:55:59 +0100

    iphone image resolved using if statement

--- src/app/components/IphoneImage.tsx
+++ src/app/components/IphoneImage.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function IphoneImage() {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    // check on mount
+    const checkSize = () => setIsDesktop(window.innerWidth >= 1024);
+    checkSize();
+
+    // listen for resize
+    window.addEventListener("resize", checkSize);
+    return () => window.removeEventListener("resize", checkSize);
+  }, []);
+
+  if (!isDesktop) return null; 
+
   return (
     <img
       src="/iphone-bg.svg"
       alt="iPhone"
       width={218}
       height={311}
-      className="absolute -right-[1rem] lg:bottom-[8rem] lg:w-[218px] lg:h-[311px] lg:z-auto"
+      className="absolute -right-[1rem] bottom-[8rem]"
     />
   );
 }
