628f2a9615d95e0ab199da27799fa240edc7f4ea
cyberrspace <xhamooel95@gmail.com>
2025-09-19 11:57:58 +0100

    hover color set for button

--- src/app/components/BasicButton.tsx
+++ src/app/components/BasicButton.tsx
@@ -2,7 +2,7 @@
 
 export default function BasicButton() {
   return (
-    <button className="w-full sm:w-[450px] h-[60px] sm:h-[72px] rounded-[64px] border border-gray-400 text-[#24477966] mt-6 sm:mt-0">
+    <button className="w-full sm:w-[450px] h-[60px] sm:h-[72px] rounded-[64px] border border-gray-400 text-[#24477966] mt-6 sm:mt-0 hover:bg-[#6E8AFC] hover:text-[#FFFFFF] transition-colors duration-200">
       Get Started for Free
     </button>
   );
--- src/app/components/ProButton.tsx
+++ src/app/components/ProButton.tsx
@@ -2,7 +2,9 @@
 
 export default function ProButton() {
   return (
-    <button className="w-full sm:w-[450px] h-[60px] sm:h-[72px] rounded-[64px] border border-gray-400 text-[#24477966] mt-6 sm:mt-0">
+    <button
+      className="w-full sm:w-[450px] h-[60px] sm:h-[72px] rounded-[64px] border border-gray-400 text-[#24477966] mt-6 sm:mt-0 hover:bg-[#6E8AFC] hover:text-[#FFFFFF] transition-colors duration-200"
+    >
       Start 14-Day Free Trial
     </button>
   );
